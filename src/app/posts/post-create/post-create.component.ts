c8df51b6075ff2da37f54cf773ce4acbb81dd64d
Elie Bizimana <ebizimana@liberty.edu>
2019-05-20 12:52:05 -0700

    Added the Post Model to all the the components

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+import {Post } from '../post.model'
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -9,10 +11,10 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class PostCreatComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreate = new EventEmitter;
+  @Output() postCreate = new EventEmitter<Post>();
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteredTitle, content: this.enteredContent
     };
     this.postCreate.emit(post);
