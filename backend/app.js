41b270cb7e966893eed0a0b38aa2aef3cc3e6ab4
Elie Bizimana <ebizimana@liberty.edu>
2019-05-21 19:22:03 -0700

    Added the posts to the app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware")
-  next()
-})
 
-app.use((rex,res,next) => {
-  res.send('Hello from express!!')
+app.use('/api/post', (rex,res,next) => {
+  const post =[
+    {
+      id:'1i34u018340kj',
+      title: "First server-side post",
+      content: 'This is coming from the server'
+  },
+  {
+    id:'1i34u018340kj',
+    title: "First server-side post",
+    content: 'This is coming from the server'
+}];
+res.status(200).json({
+  message:"Posts fetched successfully!",
+  post: post
+});
 })
 
 module.exports = app;
